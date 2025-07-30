e7936d69bd8426c1040be4acc2665cb4be67d7ad
ergXned <126957013+ergXned@users.noreply.github.com>
2025-07-30 14:59:47 +0200

    todos added

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,18 @@ use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    // peripherals init
     let p = embassy_nrf::init(Default::default());
 
+    // ble init
     let ble_builder = BleControllerBuilder::new(
         p.PPI_CH17, p.PPI_CH18, p.PPI_CH19, p.PPI_CH20, p.PPI_CH21, p.PPI_CH22, p.PPI_CH23,
         p.PPI_CH24, p.PPI_CH25, p.PPI_CH26, p.PPI_CH27, p.PPI_CH28, p.PPI_CH29, p.PPI_CH30,
         p.PPI_CH31, p.RNG, p.RTC0, p.TIMER0, p.TEMP,
     );
-
     let (sdc, mpsl) = unwrap!(ble_builder.init());
+    // todo:
+    // spawn.spawner(ble_task(sdc, mpsl));
+    // spawn.spawner(scan_matrix_task());
+    // spawn.spawner(debounce_task());
 }
