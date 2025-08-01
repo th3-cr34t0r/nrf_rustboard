596b73bd692ce48960f3adee206907035ff81751
Kushtoshi <kushtoshi@duck.com>
2025-08-01 20:10:40 +0200

    added random_generator for the stack, advertising_config

--- src/ble/ble_server.rs
+++ src/ble/ble_server.rs
@@ -1,8 +1,9 @@
-use trouble_host::prelude::characteristic::BATTERY_LEVEL;
-use trouble_host::prelude::*;
-use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
+use serde::Serialize;
+use trouble_host::prelude::{characteristic::BATTERY_LEVEL, *};
+use usbd_hid::descriptor::{SerializedDescriptor, gen_hid_descriptor, generator_prelude::*};
 
 pub const HUMAN_INTERFACE_DEVICE: BluetoothUuid16 = BluetoothUuid16::new(0x1812);
+pub const BATTERY: BluetoothUuid16 = BluetoothUuid16::new(0x180f);
 
 #[gatt_server]
 pub struct Server {
@@ -17,7 +18,7 @@ pub struct BatteryService {
 }
 #[gatt_service(uuid = service::HUMAN_INTERFACE_DEVICE)]
 pub struct HidService {
-    #[characteristic(uuid = "2a4b", read, value = KeyboardReport::desc().try_into().unwrap())]
+    #[characteristic(uuid = "2a4b", read, value = KeyboardReport::desc().try_into().expect("Failed to convert keyboard report to [u8; 67]"))]
     report_map: [u8; 67],
     #[characteristic(uuid = "2a22", read, notify)]
     input_keyboard: [u8; 8],
@@ -35,4 +36,31 @@ impl<'s> BleHidServer {
             input_keyboard: server.hid_service.input_keyboard,
         }
     }
+}
+/// KeyboardReport describes a report and its companion descriptor that can be
+/// used to send keyboard button presses to a host and receive the status of the
+/// keyboard LEDs.
+#[gen_hid_descriptor(
+    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = KEYBOARD) = {
+        (usage_page = KEYBOARD, usage_min = 0xE0, usage_max = 0xE7) = {
+            #[packed_bits 8] #[item_settings data,variable,absolute] modifier=input;
+        };
+        (logical_min = 0x00,) = {
+            #[item_settings constant,variable,absolute] reserved=input;
+        };
+        (usage_page = LEDS, usage_min = 0x01, usage_max = 0x05) = {
+            #[packed_bits 5] #[item_settings data,variable,absolute] leds=output;
+        };
+        (usage_page = KEYBOARD, usage_min = 0x00, usage_max = 0xDD) = {
+            #[item_settings data,array,absolute] keycodes=input;
+        };
+    }
+)]
+#[allow(dead_code)]
+#[derive(defmt::Format)]
+pub struct KeyboardReport {
+    pub modifier: u8,
+    pub reserved: u8,
+    pub leds: u8,
+    pub keycodes: [u8; 6],
 }
--- src/ble/mod.rs
+++ src/ble/mod.rs
@@ -1,8 +1,9 @@
-use defmt::info;
+use defmt::{error, info};
 use embassy_executor::Spawner;
 use embassy_nrf::mode::Async;
 use embassy_nrf::peripherals::{self, RNG};
 use embassy_nrf::{Peri, bind_interrupts, rng};
+use embassy_time::{Duration, Timer};
 use nrf_mpsl::Peripherals as mpsl_Peripherals;
 use nrf_mpsl::raw::{
     MPSL_CLOCK_LF_SRC_RC, MPSL_DEFAULT_CLOCK_ACCURACY_PPM, MPSL_DEFAULT_SKIP_WAIT_LFCLK_STARTED,
@@ -17,12 +18,14 @@ use nrf_sdc::{
         raw::mpsl_clock_lfclk_cfg_t,
     },
 };
+use rand::{CryptoRng, RngCore, SeedableRng};
+use rand_chacha::ChaCha12Rng;
 use static_cell::StaticCell;
 use trouble_host::gap::{CentralConfig, GapConfig};
 use trouble_host::gatt::GattConnection;
 use trouble_host::prelude::{
-    AdStructure, Advertisement, BR_EDR_NOT_SUPPORTED, Central, DefaultPacketPool,
-    LE_GENERAL_DISCOVERABLE, Peripheral, Runner, appearance,
+    AdStructure, Advertisement, AdvertisementParameters, BR_EDR_NOT_SUPPORTED, Central,
+    DefaultPacketPool, LE_GENERAL_DISCOVERABLE, Peripheral, PhyKind, Runner, TxPower, appearance,
 };
 use trouble_host::{
     Address, BleHostError, Controller, Host, HostResources, PacketPool, Stack, central,
@@ -41,7 +44,7 @@ bind_interrupts!(struct Irqs {
 });
 
 /// Default memory allocation for softdevice controller in bytes.
-const SDC_MEMORY_SIZE: usize = 1112; // bytes
+const SDC_MEMORY_SIZE: usize = 1120; // bytes
 
 pub struct BleControllerBuilder<'a> {
     sdc_p: sdc_Peripherals<'a>,
@@ -122,6 +125,7 @@ where
         (
             SoftdeviceController<'a>,
             &'static MultiprotocolServiceLayer<'a>,
+            ChaCha12Rng,
         ),
         nrf_sdc::Error,
     > {
@@ -137,7 +141,7 @@ where
             MultiprotocolServiceLayer::new(p, Irqs, Self::LFCLK_CFG)
         }?;
 
-        let sdc_rng = {
+        let mut sdc_rng = {
             static SDC_RNG: StaticCell<rng::Rng<'static, RNG, Async>> = StaticCell::new();
             SDC_RNG.init(rng::Rng::new(self.rng_p, Irqs))
         };
@@ -151,27 +155,33 @@ where
             static MPSL: StaticCell<MultiprotocolServiceLayer> = StaticCell::new();
             MPSL.init(mpsl)
         };
+        let rng = ChaCha12Rng::from_rng(&mut sdc_rng).unwrap();
         let sdc = Self::build_sdc(self.sdc_p, sdc_rng, mpsl, sdc_mem)?;
 
-        Ok((sdc, mpsl))
+        Ok((sdc, mpsl, rng))
     }
 }
 
 const BLE_NAME: &str = "nRFRustboard";
 const CONNS: usize = 2;
 const CHANNELS: usize = 1;
-const ADV_SETS: usize = 2;
+const ADV_SETS: usize = 1;
 
 type BleHostResources = HostResources<DefaultPacketPool, CONNS, CHANNELS, ADV_SETS>;
 
 /// Run BLE
-pub async fn run(
+pub async fn run<RNG>(
     sdc: SoftdeviceController<'static>,
-    mpsl: MultiprotocolServiceLayer<'static>,
+    mpsl: &'static MultiprotocolServiceLayer<'static>,
+    random_generator: &mut RNG,
     spawner: Spawner,
-) {
+) where
+    RNG: RngCore + CryptoRng,
+{
     // ble address
-    let address = Address::random([0xff, 0x16, 0x56, 0x8f, 0x24, 0xff]);
+    // let address = Address::random([0xff, 0x16, 0x56, 0x8f, 0x24, 0xff]);
+    let address: Address = Address::random([0xff, 0x8f, 0x1a, 0x05, 0xe4, 0xff]);
+    // let address =Address::random( [0x18, 0xe2, 0x21, 0x80, 0xc0, 0xc7]);
 
     let resources = {
         static RESOURCES: StaticCell<BleHostResources> = StaticCell::new();
@@ -181,7 +191,11 @@ pub async fn run(
     let stack = {
         static STACK: StaticCell<Stack<'_, SoftdeviceController<'_>, DefaultPacketPool>> =
             StaticCell::new();
-        STACK.init(trouble_host::new(sdc, resources).set_random_address(address))
+        STACK.init(
+            trouble_host::new(sdc, resources)
+                .set_random_address(address)
+                .set_random_generator_seed(random_generator),
+        )
     };
 
     let Host {
@@ -204,12 +218,14 @@ pub async fn run(
 
     // let service = BleHidServer::new(server);
 
-    // TODO: implement an advertiser
-
+    // advertiser
     loop {
         match advertise(&mut peripheral, &server).await {
             Ok(connection) => info!("Connected"),
-            Err(e) => info!("{}", e),
+            Err(e) => {
+                error!("{}", e);
+                Timer::after_millis(500).await;
+            }
         }
     }
 }
@@ -218,33 +234,55 @@ async fn advertise<'a, 'b>(
     peripheral: &mut Peripheral<'a, SoftdeviceController<'static>, DefaultPacketPool>,
     server: &'b Server<'_>,
 ) -> Result<GattConnection<'a, 'b, DefaultPacketPool>, BleHostError<Error>> {
-    let mut ad_data = [0u8; 8];
+    let mut advertiser_data = [0; 31];
+
     let ad_len = AdStructure::encode_slice(
         &[
             AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
-            AdStructure::ServiceUuids16(&[ble_server::HUMAN_INTERFACE_DEVICE.into()]),
+            AdStructure::ServiceUuids16(&[
+                ble_server::HUMAN_INTERFACE_DEVICE.to_le_bytes(),
+                ble_server::BATTERY.to_le_bytes(),
+            ]),
             AdStructure::CompleteLocalName(BLE_NAME.as_bytes()),
+            AdStructure::Unknown {
+                ty: 0x19,
+                data: &trouble_host::prelude::appearance::human_interface_device::KEYBOARD
+                    .to_le_bytes(),
+            },
         ],
-        &mut ad_data[..],
+        &mut advertiser_data[..],
     )?;
 
+    info!("[debug] ad_len: {}", ad_len);
+
+    info!("[debug] advertise_config");
+    let advertise_config = AdvertisementParameters {
+        primary_phy: PhyKind::Le2M,
+        secondary_phy: PhyKind::Le2M,
+        tx_power: TxPower::Plus8dBm,
+        interval_min: Duration::from_millis(200),
+        interval_max: Duration::from_millis(200),
+        ..Default::default()
+    };
+
+    info!("[debug] advertiser");
     let advertiser = peripheral
         .advertise(
-            &Default::default(),
+            &advertise_config,
             Advertisement::ConnectableScannableUndirected {
-                adv_data: &ad_data[..ad_len],
+                adv_data: &advertiser_data[..],
                 scan_data: &[],
             },
         )
-        .await
-        .unwrap();
+        .await?;
 
+    info!("[debug] connection");
     let connection = advertiser.accept().await?.with_attribute_server(server)?;
     Ok(connection)
 }
 
 #[embassy_executor::task]
-async fn mpsl_task(mpsl: MultiprotocolServiceLayer<'static>) -> ! {
+async fn mpsl_task(mpsl: &'static MultiprotocolServiceLayer<'static>) -> ! {
     mpsl.run().await;
 }
 
