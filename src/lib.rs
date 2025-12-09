d48b5a32d77fa9e590272bf1a64c1fbed7efa7e4
th3-cr34t0r <kushtoshi@duck.com>
2025-12-09 14:37:46 +0100

    globals under feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,26 @@ pub mod storage;
 
 use crate::{config::MATRIX_KEYS_BUFFER, matrix::KeyPos};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Watch};
-use usbd_hid::descriptor::KeyboardReport;
-
-/// Shared variable between ble and key provision tasks
-pub static KEY_REPORT: Watch<CriticalSectionRawMutex, KeyboardReport, 2> = Watch::new();
-
-/// Shared variable between ble and key provision tasks
-pub static MESSAGE_TO_PERI: Watch<CriticalSectionRawMutex, [u8; 6], 2> = Watch::new();
 
 /// Shared variable between matrix scan and key provision tasks
 pub static MATRIX_KEYS_LOCAL: Watch<CriticalSectionRawMutex, [KeyPos; MATRIX_KEYS_BUFFER], 2> =
     Watch::new();
 
+#[cfg(feature = "peripheral")]
+use usbd_hid::descriptor::KeyboardReport;
+
+#[cfg(feature = "peripheral")]
+/// Shared variable between ble and key provision tasks
+pub static KEY_REPORT: Watch<CriticalSectionRawMutex, KeyboardReport, 2> = Watch::new();
+
+#[cfg(feature = "peripheral")]
 /// Shared variable between matrix scan and key provision tasks
 pub static MATRIX_KEYS_SPLIT: Watch<CriticalSectionRawMutex, [KeyPos; MATRIX_KEYS_BUFFER], 2> =
     Watch::new();
+
+#[cfg(feature = "central")]
+/// Shared variable between ble and key provision tasks
+pub static MESSAGE_TO_PERI: Watch<CriticalSectionRawMutex, [u8; 6], 2> = Watch::new();
 
 use embassy_time::{Duration, Timer};
 
