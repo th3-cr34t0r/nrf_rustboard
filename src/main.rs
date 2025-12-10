3ac7ea8cd0b8c96915efd22bfa30fdcae2b3da4b
th3-cr34t0r <kushtoshi@duck.com>
2025-12-10 14:23:32 +0100

    battery task created and added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![no_std]
 #![no_main]
 
-use nrf_rustboard::{ble::ble_init_run, key_provision::KeyProvision};
-
 use embassy_executor::Spawner;
-use embassy_futures::join::join3;
+use embassy_futures::join::join4;
+use nrf_rustboard::battery::Battery;
+use nrf_rustboard::{ble::ble_init_run, key_provision::KeyProvision};
 
 use nrf_rustboard::peripherals::AppPeri;
 
@@ -14,15 +14,17 @@ use {defmt_rtt as _, panic_probe as _};
 async fn main(spawner: Spawner) {
     // init peripherals
     let mut p = AppPeri::new();
+    let mut battery = Battery::new(p.p04, p.saadc);
 
     // init key provision
     let mut key_provision = KeyProvision::init();
 
     // run tasks
-    let _ = join3(
+    let _ = join4(
         ble_init_run(p.ble_peri, spawner),
         p.matrix_peri.scan(),
         key_provision.run(),
+        battery.process(),
     )
     .await;
 }
